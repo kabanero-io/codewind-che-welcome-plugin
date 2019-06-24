a378bdf2824ec37a3e01d8e1e0e6164456f8dbce
Andrew Mak <makandre@ca.ibm.com>
2019-06-24 13:45:46 -0400

    initial commit
    Signed-off-by: Andrew Mak <makandre@ca.ibm.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Codewind Che Welcome Plugin
+Welcome plugin for Codewind branded version of Che. 
+
+[![Eclipse License](https://img.shields.io/badge/license-Eclipse-brightgreen.svg)](https://github.ibm.com/dev-ex/tempest/blob/master/LICENSE)
+
+Codewind welcome plugin for Che Theia to show the welcome page of Codewind after installing Codewind plugin on Che.  The welcome page contains starter information as well as documentation link to help user to under understand how to use Codewind.
+
+## Contributing
+We welcome submitting issues and contributions.
+1. [Submitting bugs](https://github.com/eclipse/codewind/issues)
+2. [Contributing](CONTRIBUTING.md)
+
+## License
+[EPL 2.0](https://www.eclipse.org/legal/epl-2.0/)
+
---
+++ src/codewind-che-welcome-plugin.ts
@@ -0,0 +1,76 @@
+/*******************************************************************************
+* Copyright (c) 2019 IBM Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     IBM Corporation - initial API and implementation
+*******************************************************************************/
+
+/**
+ * Generated using theia-plugin-generator
+ */
+
+import * as theia from '@theia/plugin';
+import * as path from 'path';
+
+function toUri(context: theia.PluginContext, resource: string): theia.Uri {
+    const file = theia.Uri.file(path.join(context.extensionPath, 'resources', resource));
+    return file.with({ scheme: 'theia-resource' });
+}
+
+function getWelcomeHTML(context: theia.PluginContext): string {
+
+    const materialCSS = toUri(context, 'materialcolors.css');
+    const welcomeCSS = toUri(context, 'welcome.css');
+    const logo = toUri(context, 'Microclimate-logo.svg');
+
+    return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; font-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; script-src 'unsafe-inline' 'self' ;"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="stylesheet" type="text/css" href="${materialCSS}"/>
+            <link rel="stylesheet" type="text/css" href="${welcomeCSS}"/>
+            <title>Welcome Page</title>
+        </head>
+        <body>
+            <div class="title"><!-- img class="logo" src="${logo}"/ -->ICAP</div>
+            <!-- div class="subtitle">Dive into a fast, incremental, intelligent, end-to-end development experience that puts developers in charge.</div -->
+        </body>
+        </html>`;
+}
+
+function createWelcomePage(context: theia.PluginContext): void {
+
+    const panel = theia.window.createWebviewPanel(
+        'WelcomePage',
+        'Welcome',
+        {
+            viewColumn: theia.ViewColumn.One,
+            preserveFocus: false
+        },
+        {
+            localResourceRoots: [
+                theia.Uri.file(path.join(context.extensionPath, 'resources'))
+            ]
+        }
+    );
+
+    panel.webview.html = getWelcomeHTML(context);
+    // panel.iconPath = theia.Uri.file(path.join(context.extensionPath, 'resources', 'favicon-11x11.png'));
+}
+
+export function start(context: theia.PluginContext) {
+    setTimeout(() => {
+        if (theia.window.visibleTextEditors.length == 0)
+            createWelcomePage(context);
+    }, 1000);
+}
+
+export function stop() {
+
+}
